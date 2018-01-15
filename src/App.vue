d02193ab5dbb0c8762760acf957310e7a923fe9d
wei-guang <1779072432@qq.com>
2018-01-15 14:30:08 +0800

    课时2.基础语法
    v-if v-for v-text 。。。

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <test></test>
+
   </div>
 </template>
 
 <script>
-
+import Test from './components/Test'
 
 export default {
   name: 'App',
   components: {
+    Test
   }
 }
 </script>
---
+++ src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="test">
+      <h1>{{title}}</h1>
+      <p>{{user.firstname}}</p>
+      <p v-text="user.lastname"></p>
+
+      <!--v-if-->
+      <p v-if="showName">{{user.firstname}}</p>
+      <p v-else>Nobody</p>
+
+      <!--v-for-->
+      <ul>
+        <li v-for="item in items">{{item.title}}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        data:function () {
+          return{
+            title:"hello vue.js!",
+            user:{
+              firstname:"paoy",
+              lastname:"Zhang"
+            },
+            showName:true,
+            items:[
+              {title:"item1"},
+              {title:"item2"},
+              {title:"item3"},
+              {title:"item4"}
+            ]
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
