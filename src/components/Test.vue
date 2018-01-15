50788b6f15c718ce03605bf22c9a256c3efea4a4
wei-guang <1779072432@qq.com>
2018-01-15 15:32:42 +0800

    课时4
    简单用户管理1

---
+++ src/components/Test.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="test">
+      <h1>{{title}}</h1>
+      <p>{{user.firstname}}</p>
+      <p v-text="user.lastname"></p>
+
+      <!--v-if-->
+      <p v-if="showName">{{user.firstname}}</p>
+      <p v-else>Nobody</p>
+
+      <!--v-for-->
+      <ul>
+        <li v-for="item in items">{{item.title}}</li>
+      </ul>
+
+      <!--v-on-->
+      <button v-on:click="greet('hello everybody')">say Greeting</button>
+
+      <!--键盘事件-->
+      <input type="text" v-on:keyup="presskey" v-on:keyup.enter="enterHit" v-on:keyup.alt.enter="enterHitAlt">
+
+      <hr>
+      <!--computed计算属性-->
+      <label>FirstName</label>
+      <input type="text" v-model="user.firstname">
+      <br>
+      <label>LastName</label>
+      <input type="text" v-model="user.lastname">
+      <br>
+      <h3>{{fullName}}</h3>
+
+      <!--props属性-->
+      <h2>{{msg}}</h2>
+
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "test",
+      props:{
+          msg:{
+            type:String,
+            default:"默认文字"
+          }
+      },
+        data:function () {
+          return{
+            title:"hello vue.js!",
+            user:{
+              firstname:"paoy",
+              lastname:"Zhang"
+            },
+            showName:true,
+            items:[
+              {title:"item1"},
+              {title:"item2"},
+              {title:"item3"},
+              {title:"item4"}
+            ]
+          }
+        },
+        methods:{
+          greet:function (greeting) {
+            alert(greeting)
+          },
+          presskey:function () {
+            console.log("pressing...")
+          },
+          enterHit:function () {
+            console.log("you press enterkey")
+          },
+          enterHitAlt:function () {
+            console.log("you press alt+enterkey")
+          }
+        },
+        computed:{
+          fullName:function () {
+            return this.user.firstname+" "+this.user.lastname;
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Users.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="users">
+    <h1>Users</h1>
+
+    <form v-on:submit="addUser">
+      <input type="text" v-model="newUser.name" placeholder="Enter name">
+      <input type="text" v-model="newUser.email" placeholder="Enter email">
+      <input type="submit" value="Submit">
+    </form>
+
+    <ul>
+      <li v-for="user in users">{{user.name}}:{{user.email}}</li>
+    </ul>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "users",
+    data:function () {
+      return{
+        newUser:{},
+        users:[
+          {
+            name:"Hemiaf Zhang",
+            email:"xxxx@mail.com",
+            contacted:false
+          },
+          {
+            name:"Henry Zhang",
+            email:"xxxx@mail.com",
+            contacted:false
+          },
+          {
+            name:"Emily Zhang",
+            email:"xxxx@mail.com",
+            contacted:false
+          }
+        ]
+      }
+    },
+    methods:{
+      addUser:function (e) {
+        //console.log("add...");
+        this.users.push({
+          name:this.newUser.name,
+          email:this.newUser.email,
+          contacted:false
+        });
+        e.preventDefault();
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
